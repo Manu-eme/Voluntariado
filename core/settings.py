dbe8e69160e1f9965268318b878e414713957911
Manuel <manuel_eme@outlook.com>
2023-11-21 23:43:05 -0300

    Prueba Correo

--- core/settings.py
+++ core/settings.py
@@ -142,10 +142,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 #Email
-
-EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', default=True)
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = os.environ.get('EMAIL_HOST')
+    EMAIL_PORT = os.environ.get('EMAIL_PORT')
+    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', default=True)
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
