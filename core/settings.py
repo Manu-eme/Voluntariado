8edc7ab77f25f07aee9f9a359c9291e4cfe207c7
Manuel <manuel_eme@outlook.com>
2023-11-22 19:31:07 -0300

    config-static

--- core/settings.py
+++ core/settings.py
@@ -130,13 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  
  
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
@@ -163,7 +163,7 @@ if not DEBUG:
     AWS_DEFAULT_ACL = 'public-read'
 
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATIC_LOCATION = 'static/'
+    STATIC_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     
